4844ee37b6c0bfe7ea1e4840e25fc072c92c8cf9
Mario Cao <mario@stampery.co>
2019-06-12 12:59:28 +0200

    working derived c

--- contracts/VRF.sol
+++ contracts/VRF.sol
@@ -71,26 +71,29 @@ contract VRF is EllipticCurve {
 
   function hash_points(uint256 h_x, uint256 h_y, uint256 gamma_x, uint256 gamma_y, uint256 u_x, uint256 u_y, uint256 v_x, uint256 v_y)
   public pure returns (bytes16) {
-    bytes memory c = new bytes(133);
+    bytes memory c = new bytes(134);
+    // Ciphersuite 0xFE
+    c[0] = byte(uint8(254));
     // Prefix 0x02
-    c[0] = byte(uint8(2));
+    c[1] = byte(uint8(2));
     // Points to Bytes
     bytes memory hBytes = point_to_bytes(h_x, h_y);
     for (uint i = 0; i < hBytes.length; i++) {
-      c[1+i] = hBytes[i];
+      c[2+i] = hBytes[i];
     }
     bytes memory gammaBytes = point_to_bytes(gamma_x, gamma_y);
     for (uint i = 0; i < gammaBytes.length; i++) {
-      c[34+i] = gammaBytes[i];
+      c[35+i] = gammaBytes[i];
     }
     bytes memory uBytes = point_to_bytes(u_x, u_y);
     for (uint i = 0; i < uBytes.length; i++) {
-      c[67+i] = uBytes[i];
+      c[68+i] = uBytes[i];
     }
     bytes memory vBytes = point_to_bytes(v_x, v_y);
     for (uint i = 0; i < vBytes.length; i++) {
-      c[100+i] = vBytes[i];
+      c[101+i] = vBytes[i];
     }
+
     // Hash bytes and truncate
     bytes32 sha = sha256(c);
     bytes16 half1;
