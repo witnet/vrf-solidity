04c734d8f36c356210fe250d54af143ffc5597da
Mario Cao <mario@stampery.co>
2019-06-28 12:41:33 +0200

    test: remove elliptic curve tests

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,8 +1,6 @@
-var EC = artifacts.require("./EllipticCurve.sol")
 var VRF = artifacts.require("./VRF.sol")
 
 module.exports = function (deployer, network, accounts) {
   console.log("Network:", network)
-  deployer.deploy(EC)
   deployer.deploy(VRF)
 }
