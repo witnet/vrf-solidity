7b76256c5c2565abc49f4b9dc3b9d9bfbd1d6e96
Mario Cao <mario@stampery.co>
2019-07-09 11:21:54 +0200

    chore: update license and dependencies

--- truffle-config.js
+++ truffle-config.js
@@ -1,16 +1,11 @@
 module.exports = {
   networks: {
     local: {
-      host: "127.0.0.1",     // Localhost (default: none)
-      port: 7545,            // Standard Ethereum port (default: none)
-      network_id: "*",       // Any network (default: none)
-    },
-    development: {
-      provider: require("ganache-cli").provider({ gasLimit: 100000000 }),
+      host: "127.0.0.1",
+      port: 8545,
       network_id: "*",
     },
   },
-  // Set default mocha options here, use special reporters etc.
   mocha: {
     // timeout: 100000
     reporter: "eth-gas-reporter",
@@ -19,17 +14,13 @@ module.exports = {
       gasPrice: 20,
     },
   },
-  // Configure your compilers
   compilers: {
     solc: {
-      // version: "0.5.1",    // Fetch exact version from solc-bin (default: truffle's version)
-      // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
-      settings: {          // See the solidity docs for advice about optimization and evmVersion
+      settings: {
         optimizer: {
           enabled: true,
           runs: 200,
         },
-      //  evmVersion: "byzantium"
       },
     },
   },
