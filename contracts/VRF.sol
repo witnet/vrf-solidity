be7b80a61647e8b5182100ff2490b3aa1a260c54
Mario Cao <mariocao@gmail.com>
2019-11-19 12:44:04 +0100

    doc: fix code documentation

--- contracts/VRF.sol
+++ contracts/VRF.sol
@@ -16,13 +16,13 @@ contract VRF {
    * Secp256k1 parameters
    */
 
-  // Generator coordinate `x` of EC equation
+  // Generator coordinate `x` of the EC curve
   uint256 constant GX = 0x79BE667EF9DCBBAC55A06295CE870B07029BFCDB2DCE28D959F2815B16F81798;
-  // Generator coordinate `y` of EC equation
+  // Generator coordinate `y` of the EC curve
   uint256 constant GY = 0x483ADA7726A3C4655DA4FBFC0E1108A8FD17B448A68554199C47D08FFB10D4B8;
   // Constant `a` of EC equation
   uint256 constant AA = 0;
-  // Constant `B` of EC equation
+  // Constant `b` of EC equation
   uint256 constant BB = 7;
   // Prime number of the curve
   uint256 constant PP = 0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEFFFFFC2F;
